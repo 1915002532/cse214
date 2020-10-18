a1cc1608fbc9591bfbb86a501f6d0e53c5ad2c6d
Rakibul Hasan Emon <73061463+1915002532@users.noreply.github.com>
2020-10-18 22:28:44 +0600

    Add files via upload

---
+++ Problem Set 1/4.c
@@ -0,0 +1,17 @@
+/* Print entire array each element in newline ......
+   Rakibul Hasan Emon
+   ID.1915002532
+*/
+#include <stdio.h>
+int main()
+{
+
+    int array[100]={1,2,3,4,5,6,7,8,9,10};
+    int size = 10, a;
+
+    for(a=0;a<size;a++)
+    {
+        printf("%d\n",array[a]);
+    }
+    return 0;
+}
