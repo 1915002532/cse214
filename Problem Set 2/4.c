715faf3bc71768d29fb7895e3c8ee7b81cd21bdb
Rakibul Hasan Emon <73061463+1915002532@users.noreply.github.com>
2020-10-18 22:37:08 +0600

    Add files via upload

---
+++ Problem Set 2/4.c
@@ -0,0 +1,44 @@
+ /*  Insert a value X (take input X from user) in the array at Kth (take input K from user) index and shift all other value to right.
+     And print the whole array ......
+     Rakibul Hasan Emon
+     ID.1915002532
+*/
+
+#include <stdio.h>
+int main()
+{
+    int array[100]={1,2,3,4,5,6,7,8,9,10};
+    int size = 10, a , X;
+    int k;
+    printf("Original Array : ");
+    for(a=0;a<size;a++)
+    {
+        printf("%d ",array[a]);
+    }
+    printf("\n\nInput the value of X : ");
+    scanf("%d",&X);
+
+    printf("\nSelect the index position of kth (0 to 9) : ");
+    scanf("%d",&k);
+
+    printf("\nAfter insert value : ");
+
+    for(a=size;a>=k;a--)
+    {
+       array[a] = array[a-1];
+    }
+
+    size++;
+    array[k]=X;
+
+    for(a=0;a<size;a++)
+    {
+        printf("%d ",array[a]);
+    }
+
+     printf("\n");
+
+    return 0;
+
+
+}
